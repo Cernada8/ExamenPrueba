be7d8daf0704776e9093fc2e6539865146ab5a96
Cernada8 <150514997+Cernada8@users.noreply.github.com>
2023-11-30 10:16:05 +0100

    Comentario desde GitHub

--- PruebasParaExamen/src/PaqueteExamenes/ClaseDeExamen.java
+++ PruebasParaExamen/src/PaqueteExamenes/ClaseDeExamen.java
@@ -4,6 +4,7 @@ package PaqueteExamenes;
 public class ClaseDeExamen {
 	public static void main(String[]args) {
 		System.out.println("Hola yo soy Juan, pero me llaman Carlos");
+		//esto lo añado desde GitHub
 	}
 
 }
